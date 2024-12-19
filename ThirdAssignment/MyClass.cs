5643c7384f89a25db83569ccc74bb2402907188a
MARIAM YAHIA <mariam0yehia@gmail.com>
2024-12-19 23:10:41 +0200

    done

---
+++ ThirdAssignment/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdAssignment
+{
+    internal class MyClass
+    {
+        public int Value;
+    }
+}
