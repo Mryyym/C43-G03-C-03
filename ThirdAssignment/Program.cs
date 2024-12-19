c560227e623a7b0f35ecded6eefca9018437ce11
MARIAM YAHIA <mariam0yehia@gmail.com>
2024-12-19 16:27:02 +0200

    ThirdAssignment

---
+++ ThirdAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ThirdAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+
+
+            #endregion
+
+
+            #region Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example
+
+            #endregion
+
+
+            #region Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+            #endregion
+
+
+            #region Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+
+            #endregion
+
+
+            #region Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:
+
+
+            #endregion
+
+
+            #region 6-Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+
+
+            #endregion
+
+
+            #region 7-Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
+
+
+            #endregion
+
+
+            #region 8-Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
+
+
+            #endregion
+           
+        }
+
+    }
+}
